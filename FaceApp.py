c180f35b489aa167cd013955609ab2ba2b33eed4
rabi320 <ryonatan6@gmail.com>
2021-10-11 17:28:04 +0300

    FaceApp initiation

---
+++ FaceApp.py
@@ -0,0 +1,15 @@
+import streamlit as st
+#title
+st.markdown("""
+# Face App: try to outsmart The computer!
+The rules are simple:
+
+1. you are given a black and white low quality image.
+2. predict the Ethincity (white, black, asian or male) - 3 points
+3. predict the gender - 2 points.
+4. predict the age group (child (1-18), young adult(19-35), adult(36-50), middle aged(51-70) or elder(71-120)) - 5 points.
+5. the higher score wins!
+
+so who's vision is better? 
+""")
+st.markdown('---')
