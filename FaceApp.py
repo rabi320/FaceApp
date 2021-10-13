7c5884e623ce344271540800ae49cf7b2731a95e
rabi320 <74452749+rabi320@users.noreply.github.com>
2021-10-13 08:46:01 +0300

    Add files via upload

--- FaceApp.py
+++ FaceApp.py
@@ -4,11 +4,11 @@ st.markdown("""
 # Face App: try to outsmart The computer!
 The rules are simple:
 
-1. you are given a black and white low quality image.
-2. predict the Ethincity (white, black, asian or male) - 3 points
-3. predict the gender - 2 points.
-4. predict the age group (child (1-18), young adult (19-35), adult (36-50), middle aged (51-70) or elder (71-120)) - 5 points.
-5. the higher score wins!
+1. You are given a black and white low quality image.
+2. Predict the Ethincity (white, black, asian or male) - 3 points
+3. Prdict the gender - 2 points.
+4. PrTedict the age group (child (1-18), young adult (19-35), adult (36-50), middle aged (51-70) or elder (71-120)) - 5 points.
+5. The higher score wins!
 
 so who's vision is better? 
 """)
